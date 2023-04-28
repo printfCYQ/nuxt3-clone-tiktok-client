6759681b2c2e8ed9e6419f47aad39c6a4a95bd71
printfCYQ <459987870@qq.com>
2023-04-28 13:29:45 +0800

    sidenav

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,8 +1,15 @@
 <template>
-  <div
-  >
+  <div>
     <TopNav />
+    <div :class="route.fullPath === '/' ? 'max-w-[1140px]' : ''"
+      class="flex justify-between mx-auto w-full lg:px-2.5 px-0">
+      <div>
+        <SideNavMain />
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute();
+</script>
